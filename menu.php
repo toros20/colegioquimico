b074326bc99e6d53994f1bba11cd3c8f5dc8cc30
Marvin Toro <marvinlink07@gmail.com>
2019-12-16 15:47:42 -0600

    main menu

---
+++ menu.php
@@ -0,0 +1,140 @@
+   <!-- Header Area Start Here -->
+   <header>
+              <div id="header2" class="header4-area">
+                <div class="header-top-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                                <div class="header-top-left">
+                                    <div class="logo-area">
+                                        <a href="index.php"><img class="img-responsive" src="img/logo-primary.png" alt="logo"></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">
+                                <div class="header-top-right">
+                                    <ul>
+                                        <li><i class="fa fa-phone" aria-hidden="true"></i> 504 + 2221-4474 </li>
+                                        <li><i class="fa fa-envelope" aria-hidden="true"></i>info@sanjosedelacarmen.edu.hn</li>
+                                        <li>
+                                            <a class="login-btn-area" href="#" id="login-button"><i class="fa fa-lock" aria-hidden="true"></i> Ingresar</a>
+                                            <div class="login-form" id="login-form" style="display: none;">
+                                                <div class="">(Solo Empleados de TI)</div>
+                                                <br>
+                                                <form>
+                                                    <label>Nombre de usuario *</label>
+                                                    <input type="text" placeholder="Nombre de usuario" />
+                                                    <label>Clave *</label>
+                                                    <input type="Clave" placeholder="Clave" />
+                                                    <button class="default-big-btn" type="submit" value="Ingresar">Ingresar</button>
+                                                   
+                                                </form>
+                                            </div>
+                                        </li>
+                                        <li><a href="matricula.php" class="apply-now-btn2">Matr&iacute;cula</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="main-menu-area bg-primary" id="sticker">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <nav id="desktop-nav">
+                                    <ul>
+                                        <li class="active"><a href="index.php">Inicio</a></li>
+                                        <li><a href="#">Nosotros</a>
+                                            <ul>
+                                                <li><a href="historia.php">Historia</a></li>
+                                                <li><a href="mision_vision.php">Misi&oacute; y Visi&oacute;n</a></li>
+                                                <li><a href="proyecto.php">Proyecto Carmelitano</a></li>
+                                            </ul>
+                                        </li> 
+                                        <li><a href="../virtual/public/excelencia">Excelencia</a></li>  
+
+                                        <li><a href="#">Areas</a>
+                                            <ul>
+                                                <li><a href="prebasica.php">Pre-B&aacute;sica</a></li>
+                                                <li> <a href="basica.php">B&aacute;sica</a></li>
+                                                <li><a href="media.php">Media</a></li>
+                                                <li> <a href="https://www.facebook.com/polideportivosanjose/">Polideportivo</a></li>
+                                                
+                                             </ul>
+                                        </li>
+
+                                         <li><a href="matricula.php">Matr&iacute;cula</a></li>
+
+                                         <li><a translate="_blank" href="/virtual/public/">Plataforma</a>
+                                            <ul>
+                                                
+                                               <!--  <li><a target="_blank" href="https://elecciones2019.000webhostapp.com/elecciones_primaria.php">Primaria</a>
+                                                <li><a target="_blank" href="https://elecciones2019.000webhostapp.com/elecciones.php">Secundaria</a> -->
+                                            </ul>
+                                        </li> 
+                                        
+                                         
+                                        <li><a href="contacto.php">Cont&aacute;cto</a> </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Mobile Menu Area Start -->
+            <div class="mobile-menu-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mobile-menu">
+                                <nav id="dropdown">
+                                     <ul>
+                                        <li class="active"><a href="index.php">Inicio</a></li>
+                                        <li><a href="#">Nosotros</a>
+                                            <ul>
+                                                <li><a href="historia.php">Historia</a></li>
+                                                <li><a href="mision_vision.php">Misi&oacute; y Visi&oacute;n</a></li>
+                                                <li><a href="proyecto.php">Proyecto Carmelitano</a></li>
+                                            </ul>
+                                        </li> 
+
+                                        <li><a href="../virtual/public/excelencia">Excelencia</a></li>  
+
+                                        <li><a href="#">Areas</a>
+                                            <ul>
+                                                 <li><a href="prebasica.php">Pre-B&aacute;sica</a></li>
+                                                <li> <a href="basica.php">B&aacute;sica</a></li>
+                                                <li><a href="media.php">Media</a></li>
+                                                <li> <a target="_blank" href="https://www.facebook.com/polideportivosanjose/">Polideportivo</a></li>
+                                                
+                                             </ul>
+                                        </li>
+
+                                        <li><a href="matricula.php">Matr&iacute;cula</a></li>
+
+                                        <li><a target="_blank" href="/virtual/public/">Plataforma</a></li>
+                                        
+                                         
+                                           <!--  <ul>
+                                                <li><a target="_blank" href="pre_basica">Pre-B&aacute;sica</a></li>
+                                                <li> <a target="_blank" href="https://lms30.santillanacompartir.com/login/compartir">B&aacute;sica</a></li>
+                                                <li><a target="_blank" href="geo/users/login" >Media</a></li>                                               
+                                             </ul> -->
+                                        </li>
+                                            
+                                        <li><a href="contacto.php">Cont&aacute;cto</a> </li>
+                                            
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Mobile Menu Area End -->
+
+                   </header> 
+        <!-- Header Area End Here -->
