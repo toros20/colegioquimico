8e920c61ebd9c437f65c3e955de6b3e0bdce9579
Marvin Toro <marvinlink07@gmail.com>
2019-12-16 16:09:50 -0600

    created slider

---
+++ footer.php
@@ -0,0 +1,30 @@
+ <!-- jquery-->
+ <script src="js/jquery-2.2.4.min.js" type="text/javascript"></script>
+    <!-- Plugins js -->
+    <script src="js/plugins.js" type="text/javascript"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- WOW JS -->
+    <script src="js/wow.min.js"></script>
+    <!-- Nivo slider js -->
+    <script src="vendor/slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+    <script src="vendor/slider/home.js" type="text/javascript"></script>
+    <!-- Owl Cauosel JS -->
+    <script src="vendor/OwlCarousel/owl.carousel.min.js" type="text/javascript"></script>
+    <!-- Meanmenu Js -->
+    <script src="js/jquery.meanmenu.min.js" type="text/javascript"></script>
+    <!-- Srollup js -->
+    <script src="js/jquery.scrollUp.min.js" type="text/javascript"></script>
+    <!-- jquery.counterup js -->
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <!-- Countdown js -->
+    <script src="js/jquery.countdown.min.js" type="text/javascript"></script>
+    <!-- Isotope js -->
+    <script src="js/isotope.pkgd.min.js" type="text/javascript"></script>
+    <!-- Magic Popup js -->
+    <script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+    <!-- Gridrotator js -->
+    <script src="js/jquery.gridrotator.js" type="text/javascript"></script>
+    <!-- Custom Js -->
+    <script src="js/main.js" type="text/javascript"></script>
--- index.php
+++ index.php
@@ -17,6 +17,61 @@
             //importamos el header y el menu 
             include('menu.php');
         ?>
+    
+ <!-- Slider 1 Area Start Here -->
+ <div class="slider1-area ">
+            <div class="bend niceties preview-1">
+                <div id="ensign-nivoslider-3" class="slides">
+                     <img src="img/slider/1-1.jpg" alt="Colegio Químico y Farmacéutico de Honduras" title="#slider-direction-3" />
+                     <img src="img/slider/1-2.jpg" alt="Colegio Químico y Farmacéutico de Honduras" title="#slider-direction-1" />
+                    <img src="img/slider/1-3.jpg" alt="Colegio Químico y Farmacéutico de Honduras" title="#slider-direction-2" />                   
+                    <img src="img/slider/2-1.jpg" alt="Colegio Químico y Farmacéutico de Honduras" title="#slider-direction-3" />
+                    <img src="img/slider/2-2.jpg" alt="Colegio Químico y Farmacéutico de Honduras" title="#slider-direction-3" />
+                </div>
+                <div id="slider-direction-1" class="t-cn slider-direction">
+                    <div class="slider-content s-tb slide-1">
+                        <div class="title-container s-tb-c">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div id="slider-direction-2" class="t-cn slider-direction">
+                    <div class="slider-content s-tb slide-2">
+                        <div class="title-container s-tb-c">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div id="slider-direction-3" class="t-cn slider-direction">
+                    <div class="slider-content s-tb slide-3">
+                        <div class="title-container s-tb-c">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div id="slider-direction-4" class="t-cn slider-direction">
+                    <div class="slider-content s-tb slide-4">
+                        <div class="title-container s-tb-c">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div id="slider-direction-5" class="t-cn slider-direction">
+                    <div class="slider-content s-tb slide-5">
+                        <div class="title-container s-tb-c">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Slider 1 Area End Here -->
      
     </div> <!--End Of Wraper-->
 </body>
+   <!-- Main Body Area End Here -->
+   <?php
+        //importamos el footer con los js 
+        include('footer.php');
+    ?>
+</html>
